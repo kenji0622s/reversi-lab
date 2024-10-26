d0bc40675f57200ed3cda32634802df8f69e973d
kenji0622s <kenji0622s@gmail.com>
2024-10-26 12:03:19 +0900

    grid

--- resources/js/Pages/Play.vue
+++ resources/js/Pages/Play.vue
@@ -144,24 +144,24 @@ function checkSingleDirectionCells(singleDirectionCells) {
     <div class="grid grid-cols-8 w-80 aspect-square border border-black mx-auto bg-emerald-500">
         <template v-for="row in 8">
             <template v-for="column in 8">
-                <div :id="[row, column]" class="aspect-square border border-gray-500 flex justify-center items-center"
+                <div :id="[row, column]" class="aspect-square border border-gray-500 relative"
                     @click="selectCell([row, column])">
                     <span v-if="whiteCells.some(cell => cell[0] === row && cell[1] === column)"
-                        class="w-4/5 h-4/5 rounded-full bg-neutral-50 text-[10px] text-black flex justify-center items-center">
+                        class="w-4/5 h-4/5 rounded-full bg-neutral-50 text-[10px] text-black flex justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                         {{ row }},{{ column }}
                     </span>
                     <span v-else-if="blackCells.some(cell => cell[0] === row && cell[1] === column)"
-                        class="w-4/5 h-4/5 rounded-full bg-neutral-950 text-[10px] text-white flex justify-center items-center">
+                        class="w-4/5 h-4/5 rounded-full bg-neutral-950 text-[10px] text-white flex justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                         {{ row }},{{ column }}
                     </span>
                     <span
                         v-else-if="whiteAvailableCells.some(whiteAvailableCell => whiteAvailableCell[0] === row && whiteAvailableCell[1] === column)"
-                        :class="['w-4/5', 'h-4/5', 'text-[10px]', 'text-black', 'flex', 'justify-center', 'items-center', turn === 'white' ? 'bg-amber-200 opacity-70' : '']">
+                        :class="['w-4/5', 'h-4/5', 'text-[10px]', 'text-black', 'flex', 'justify-center', 'items-center', 'absolute', 'top-1/2', 'left-1/2', '-translate-x-1/2', '-translate-y-1/2', turn === 'white' ? 'bg-amber-200 opacity-70' : '']">
                         {{ row }},{{ column }}
                     </span>
                     <span
                         v-else-if="blackAvailableCells.some(blackAvailableCell => blackAvailableCell[0] === row && blackAvailableCell[1] === column)"
-                        :class="['w-4/5', 'h-4/5', 'text-[10px]', 'text-black', 'flex', 'justify-center', 'items-center', turn === 'black' ? 'bg-amber-200 opacity-70' : '']">
+                        :class="['w-4/5', 'h-4/5', 'text-[10px]', 'text-black', 'flex', 'justify-center', 'items-center', 'absolute', 'top-1/2', 'left-1/2', '-translate-x-1/2', '-translate-y-1/2', turn === 'black' ? 'bg-amber-200 opacity-70' : '']">
                         {{ row }},{{ column }}
                     </span>
                     <span v-else class="w-full h-full text-[10px] text-black flex justify-center items-center">
