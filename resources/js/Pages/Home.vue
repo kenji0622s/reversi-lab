c00cd72f3f8a39839930d183908d4fcf163bd8bf
kenji0622s <kenji0622s@gmail.com>
2024-10-28 17:37:53 +0900

    HOMEに影追加

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -11,19 +11,23 @@ import { Head, Link } from '@inertiajs/vue3';
         <template #title>Home</template>
         <div class="flex flex-col gap-4 items-center mt-8">
             <Link :href="route('brains.index')"
-                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60">Brains</Link>
+                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60 shadow-md">Brains
+            </Link>
             <Link :href="route('watch')"
-                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60">
-            Watch</Link>
+                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60 shadow-md">Watch
+            </Link>
             <Link :href="route('records.create')"
-                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60">Simulation</Link>
+                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60 shadow-md">Simulation
+            </Link>
             <Link :href="route('records.index')"
-                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60">Records</Link>
+                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60 shadow-md">Records
+            </Link>
             <Link :href="route('play')"
-                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60">Play
+                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60 shadow-md">Play
             </Link>
             <Link :href="route('dashboard')" v-if="$page.props.auth.user"
-                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60">Dashboard</Link>
+                class="bg-emerald-500 text-lg font-bold text-center text-white p-2 rounded-md w-60 shadow-md">Dashboard
+            </Link>
         </div>
     </BasicLayout>
 </template>
