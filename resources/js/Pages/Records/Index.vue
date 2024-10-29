19c70031f75a8ba2579f4ae903828641d6293493
kenji0622s <kenji0622s@gmail.com>
2024-10-30 00:24:31 +0900

    ...

--- resources/js/Pages/Records/Index.vue
+++ resources/js/Pages/Records/Index.vue
@@ -10,8 +10,8 @@ defineProps({
     messages: Object,
 });
 
-const show_brains_flag = ref(true);
-const show_users_flag = ref(false);
+const show_brains_flag = ref(false);
+const show_users_flag = ref(true);
 
 </script>
 
@@ -24,13 +24,44 @@ const show_users_flag = ref(false);
         <div class="w-11/12 mx-auto mt-4">
             <div class="flex items-center justify-center gap-4 mb-4">
                 <button class="border-2 border-neutral-300 py-2 px-4 rounded-md shadow-sm text-sm font-bold"
-                    :class="{ 'bg-emerald-500 text-white':  show_brains_flag, 'bg-neutral-100 text-neutral-900': !show_brains_flag }"
-                    @click="show_brains_flag = true; show_users_flag = false">Brains</button>
-                <button class="border-2 border-neutral-300 py-2 px-4 rounded-md shadow-sm text-sm font-bold"
                     :class="{ 'bg-emerald-500 text-white': show_users_flag, 'bg-neutral-100 text-neutral-900': !show_users_flag }"
                     @click="show_brains_flag = false; show_users_flag = true">Users</button>
+                <button class="border-2 border-neutral-300 py-2 px-4 rounded-md shadow-sm text-sm font-bold"
+                    :class="{ 'bg-emerald-500 text-white':  show_brains_flag, 'bg-neutral-100 text-neutral-900': !show_brains_flag }"
+                    @click="show_brains_flag = true; show_users_flag = false">Brains</button>
             </div>
             <div class="bg-neutral-100 border-2 border-neutral-300 p-4 rounded-md shadow-sm text-center text-sm">
+                <table class="w-full" v-if="show_users_flag">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>User Name</th>
+                            <th>BrainID</th>
+                            <th>Winner</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="userRecord in userRecords" :key="userRecord.id">
+                            <td>{{ userRecord.id }}</td>
+                            <td>
+                                {{ users.find(user => user.id === userRecord.user_id)?.name || 'Guest' }}
+                            </td>
+                            <td>{{ userRecord.brain_id }}</td>
+                            <td>
+                                <template v-if="userRecord.result == 'win'">
+                                    勝ち
+                                </template>
+                                <template v-else-if="userRecord.result == 'lose'">
+                                    負け
+                                </template>
+                                <template v-else>引き分け</template>
+                             
+                            </td>
+                            <td>{{ dayjs(userRecord.created_at).format('YYYY/MM/DD') }}</td>
+                        </tr>
+                    </tbody>
+                </table>
                 <table class="w-full" v-if="show_brains_flag">
                     <thead>
                         <tr>
@@ -58,39 +89,6 @@ const show_users_flag = ref(false);
                         </tr>
                     </tbody>
                 </table>
-
-                <table class="w-full" v-if="show_users_flag">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>User ID</th>
-                            <th>BrainID</th>
-                            <th>Winner</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="userRecord in userRecords" :key="userRecord.id">
-                            <td>{{ userRecord.id }}</td>
-                            <td>
-                                {{ users.find(user => user.id === userRecord.user_id)?.name || 'Guest' }}
-                            </td>
-                            <td>{{ userRecord.brain_id }}</td>
-                            <td>
-                                <template v-if="userRecord.result == 'win'">
-                                    user
-                                </template>
-                                <template v-else-if="userRecord.result == 'lose'">
-                                    brain
-                                </template>
-                                <template v-else>-</template>
-                             
-                            </td>
-                            <td>{{ dayjs(userRecord.created_at).format('YYYY/MM/DD') }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-
             </div>
         </div>
         <div class="fixed bottom-4 right-4" v-if="$page.props.auth.user && $page.props.auth.user.is_admin && show_brains_flag">
